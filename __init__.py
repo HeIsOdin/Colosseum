7f8bc6e0881ba31178c7af179b2c86c07e5a5ac2
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2026-06-29 19:38:04 -0500

    Add initial application setup with Flask login and Redis connection

---
+++ __init__.py
@@ -0,0 +1,13 @@
+from flask_login import LoginManager
+from dotenv import load_dotenv
+from armamentarium import redis_connect
+
+load_dotenv() # remove for prod
+
+NAME = "Hypogeum"
+login_manager = LoginManager()
+REDIS_CLIENT = redis_connect()
+
+USER_STATUS = ['active', 'verified', 'suspended', 'banned']
+DIFFICULTY_LEVELS = ['Sanity Check', 'Easy', 'Medium', 'Hard']
+CATEGORIES = ['Warmup', 'Web', 'Crypto', 'Forensics', 'Pwn', 'Misc']
